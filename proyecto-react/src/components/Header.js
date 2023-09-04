8be04235be6635555e2e7df96b6550236311e27d
JuanGomezD <jgomezdelgado@udesa.edu.ar>
2023-09-04 15:58:24 -0300

    PAKITAA

--- proyecto-react/src/components/Header.js
+++ proyecto-react/src/components/Header.js
@@ -1,14 +1,32 @@
 import React from "react";
 
 function Header (){
-    return(
-        <div>
+    return (
+        <React.Fragment>
 
-
-
-
-        </div>
-    )
+            <section>
+               
+                <img class="logo" src="./Albert-C:\Users\deds_\OneDrive\Documentos\Prog 3\Proyecto Integrador\progra3-react\proyecto-react\public\Img\Albert fachero.jpg.jpg" alt="Logo" srcset=""/>
+                <br/>
+                <br/>
+               
+                <article class="formulario">
+                    <form action="./search-results.html" method="get">
+                        <input class="cuadro_buscar" type="text" name="Buscador" value="" placeholder=" Iniciar busqueda"/>
+                        <button class="buscarbotton" type="submit"> Buscar</button>
+                    </form> 
+                </article>
+                <br/>
+                <br/>
+               
+                <article class="genero-titulo-div">
+                    <a class="titulo-href" href="./genres.html">GENEROS</a>
+                    <br/>
+                    <a class="titulo-href" href="./favoritos.html">FAVORITOS</a>
+                </article>
+            </section>     
+        </React.Fragment>    
+    );
 }
 
 export default Header; 
