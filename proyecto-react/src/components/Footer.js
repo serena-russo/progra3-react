59f1aee2ff14a054270e3be739fb5614f25ba6f8
serena-russo <srusso@udesa.edu.ar>
2023-09-04 15:02:45 -0300

    footer

---
+++ proyecto-react/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer (){
+    return (
+        <section>
+            <ul className="">
+                <li>Francisco St John</li>
+                <li>Juan Manuel Lopez</li>
+                <li>Serena Russo</li>
+            </ul>
+        </section>
+    )
+}
+
+export default Footer;
---
+++ proyecto-react/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header (){
+    return(
+        <div>
+
+
+
+
+        </div>
+    )
+}
+
+export default Header; 
