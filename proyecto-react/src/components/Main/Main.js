fac5065335217a3f95abbfbd44e37223f12b174a
serena-russo <srusso@udesa.edu.ar>
2023-09-11 13:46:24 -0300

    mas comentarios

--- proyecto-react/src/components/Main/Main.js
+++ proyecto-react/src/components/Main/Main.js
@@ -10,6 +10,7 @@ class Main extends Component {
     }
 
     componentDidMount(){
+        // console.log('DidMount'); //creo que se hacia asi para ver si funciona 
         let url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc'
         
         fetch(url)
@@ -20,8 +21,9 @@ class Main extends Component {
             .catch( error => console.log(error))
             )
         }
-        
     }
+
+/// hay que hacer otro component did mount con la otra url de peliculas trending? 
 
     render() {
         return (
