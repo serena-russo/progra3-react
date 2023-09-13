54bb8a3b5526695b9f12c3d1b30684645480d0e1
serena-russo <srusso@udesa.edu.ar>
2023-09-13 16:08:27 -0300

    hice pag notfound, me falta agregarla en app con switch

--- proyecto-react/src/App.js
+++ proyecto-react/src/App.js
@@ -1,6 +1,9 @@
+import {Route, Switch} from "react-router-dom";
+
 import Header from "./components/Header/Header"
 import Footer from "./components/Footer/Footer"
 import PeliculasTrending from "./components/PeliculasTrending/PeliculasTrending";
+import NotFound from "./components/NotFound/NotFound";
 
 
 function App() {
---
+++ proyecto-react/src/components/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function NotFound() {
+    console.log('Not found');
+    
+    return(
+        <section>
+        <h1>Not Found</h1>
+        </section>
+    )
+}
+
+export default NotFound;
